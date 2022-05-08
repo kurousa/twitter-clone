a4eb66744152b716c5ba0e38ae0e478ffa758548
kurousa <r.takahashi.1026@gmail.com>
2022-05-08 13:15:12 +0900

    implements View for tweet

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,15 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Tweet } from '../typings'
 import TweetBox from './TweetBox'
+import TweetComponent from '../components/Tweet'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+
+function Feed({ tweets }: Props) {
   return (
     <div className='col-span-7 lg:col-span-5 border-x'>
       <div className='flex items-center justify-between'>
@@ -13,6 +20,13 @@ function Feed() {
       {/* TweetBox */}
       <div>
         <TweetBox/>
+      </div>
+
+      {/* Feeds */}
+      <div>
+        {tweets.map((tweet) => (
+          <TweetComponent key={tweet._id} tweet={tweet} />
+        ))}
       </div>
     </div>
   )
